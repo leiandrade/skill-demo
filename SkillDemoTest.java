7a4942ade035ee3e2cf63016fb1101b815a9e2c8
leimandrade <79082487+leimandrade@users.noreply.github.com>
2022-02-04 14:56:40 -0800

    updated numbers
    corrected the input!

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,7 +5,7 @@ public class SkillDemoTest {
     // need to write code about division and test codes
     @Test
     public void divide() {       
-        assertEquals(2, SkillDemo.divide(4, 1));    
+        assertEquals(2, SkillDemo.divide(4, 2));    
     }
  
     
